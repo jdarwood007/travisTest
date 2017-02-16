f347f0a78f2dceb54719a7d4bffa78b8ae124fc2
jdarwood007 <unmonitored+github@sleepycode.com>
2017-02-16 15:45:33 -0800

    Test this
    Signed-off-by: jdarwood007 <unmonitored+github@sleepycode.com>

--- check-signed-off.php
+++ check-signed-off.php
@@ -11,85 +11,137 @@
  * @version 2.1 Beta 3
  */
 
+// Debug stuff.
+global $debugMsgs, $debugMode;
+$debugMode = true;
+
 $message = trim(shell_exec('git show -s --format=%B HEAD'));
 $lines = explode("\n", trim(str_replace("\r", "\n", $message)));
 $lastLine = $lines[count($lines) - 1];
 
-// This is debugging stuff for now to figure out why Travis can fail to get us the right info about a signed off commit.
-echo "--DEBUG MASTER B --\n";
-shell_exec('git show -s --format=%B HEAD');
-echo "\n--DEBUG MASTER B --\n";
+// First, lets do a basic test.  This is non GPG signed commits.
+$signedoff = find_signed_off();
 
-echo "--DEBUG MASTER B2 --\n";
-shell_exec('git show -s --format=%b HEAD');
-echo "\n--DEBUG MASTER B2 --\n";
+// Now Try to test for the GPG if we don't have a message.
+if (empty($signedoff))
+	$signedoff = find_gpg();
 
-echo "--DEBUG MASTER N --\n";
-shell_exec('git show -s --format=%N HEAD');
-echo "\n--DEBUG MASTER N --\n";
-
-echo "--DEBUG MASTER d --\n";
-shell_exec('git show -s --format=%d HEAD');
-echo "\n--DEBUG MASTER d --\n";
-
-echo "--DEBUG MASTER H --\n";
-shell_exec('git show -s --format=%H HEAD');
-echo "\n--DEBUG MASTER H --\n";
-
-echo "--DEBUG MASTER T --\n";
-shell_exec('git show -s --format=%T HEAD');
-echo "\n--DEBUG MASTER T --\n";
-
-echo "--DEBUG MASTER P --\n";
-shell_exec('git show -s --format=%P HEAD');
-echo "\n--DEBUG MASTER P --\n";
-
-// Did we find a merge?
-if (preg_match('~Merge ([A-Za-z0-9]{40}) into ([A-Za-z0-9]{40})~i', $lastLine, $merges))
+// Nothing?  Well darn.
+if (empty($signedoff))
 {
-	echo 'Message contains a merge, trying to find parent [' . $lastLine . ']' . '[' . $message . ']' . "\n";
-	$message = trim(shell_exec('git show -s --format=%B ' . $merges[1]));	
+	// Debugging, eh?
+	if ($debugMode)
+	{
+		echo "\n---DEBUG MSGS START ---\n";
+		var_dump($debugMsgs);
+		echo "\n---DEBUG MSGS END ---\n";
+	}
+
+	die('Error: Signed-off-by not found in commit message' . "\n");
+}
+elseif ($debugMode)
+	debugPrint('Valid signed off found' . "\n");
+
+// Find a commit by Signed Off
+function find_signed_off($commit = 'HEAD', $childs = array(), $level = 0)
+{
+	global $debugMsgs;
+
+	// To many recrusions here.
+	if ($level > 10)
+	{
+		$debugMsgs[$commit . ':' . time()] = array('error' => 'Recurision limit');
+		debugPrint('Recusion limit exceeded on find_signed_off');
+		return false;
+	}
+
+	// What string tests should we look for?
+	$stringTests = array('Signed-off-by:', 'Signed by');
+
+	// Get message data and clean it up, should only need the last line.
+	$message = trim(shell_exec('git show -s --format=%B ' . $commit));
 	$lines = explode("\n", trim(str_replace("\r", "\n", $message)));
 	$lastLine = $lines[count($lines) - 1];
 
+	// Debug info.
+	debugPrint('Testing Line [' . $lastLine . ']');
 
-	// This is debugging stuff for now to figure out why Travis can fail to get us the right info about a signed off commit.
-	echo "--DEBUG SECONDARY B --\n";
-	shell_exec('git show -s --format=%B ' . $merges[1] . '');
-	echo "\n--DEBUG SECONDARY B --\n";
+	// loop through each test and find one.
+	$result = false;
+	foreach ($stringTests as $testedString)
+	{
+		$result = stripos($lastLine, $testedString);
 
-	echo "--DEBUG SECONDARY B2 --\n";
-	shell_exec('git show -s --format=%b ' . $merges[1] . '');
-	echo "\n--DEBUG SECONDARY B2 --\n";
+		// We got a result.
+		if ($result >= 0)
+		{
+			debugPrint('Found Result [' . $testedString . ']');
+			break;
+		}
+	}
 
-	echo "--DEBUG SECONDARY N --\n";
-	shell_exec('git show -s --format=%N ' . $merges[1] . '');
-	echo "\n--DEBUG SECONDARY N --\n";
+	// Debugger.
+	$debugMsgs[$commit . ':' . time()] = array(
+		// Raw body.
+		'B' => shell_exec('git show -s --format=%B ' . $commit),
+		// Body.
+		'b2' => shell_exec('git show -s --format=%b ' . $commit),
+		// Commit notes.
+		'N' => shell_exec('git show -s --format=%N ' . $commit),
+		// Ref names.
+		'd' => shell_exec('git show -s --format=%d ' . $commit),
+		// Commit hash.
+		'H' => shell_exec('git show -s --format=%H ' . $commit),
+		// Tree hash.
+		'T' => shell_exec('git show -s --format=%T ' . $commit),
+		// Parent hash.
+		'P' => shell_exec('git show -s --format=%P ' . $commit),
+		// Result.
+		'result' => $result,
+		// Last tested string, or the correct string.
+		'testedString' => $testedString,
+	);
 
-	echo "--DEBUG SECONDARY d --\n";
-	shell_exec('git show -s --format=%d ' . $merges[1] . '');
-	echo "\n--DEBUG SECONDARY d --\n";
+	// No result and found a merge? Lets go deeper.
+	if (empty($result) && preg_match('~Merge ([A-Za-z0-9]{40}) into ([A-Za-z0-9]{40})~i', $lastLine, $merges))
+	{
+		debugPrint('Found Merge, attempting to get more parent commit: ' . $merges[1]);
 
-	echo "--DEBUG SECONDARY H --\n";
-	shell_exec('git show -s --format=%H ' . $merges[1] . '');
-	echo "\n--DEBUG SECONDARY H --\n";
+		return find_signed_off($merges[1], array_merge(array($merges[1]), $childs), ++$level);
+	}
 
-	echo "--DEBUG SECONDARY T --\n";
-	shell_exec('git show -s --format=%T ' . $merges[1] . '');
-	echo "\n--DEBUG SECONDARY T --\n";
-
-	echo "--DEBUG SECONDARY P --\n";
-	shell_exec('git show -s --format=%P ' . $merges[1] . '');
-	echo "\n--DEBUG SECONDARY P --\n";
+	return $result !== false;
 }
 
-$result = stripos($lastLine, 'Signed-off-by:');
-if ($result === false)
+// Find a commit by GPG
+function find_gpg($commit = 'HEAD', $childs = array())
 {
-	// Try 2.
-	$result2 = stripos($lastLine, 'Signed by');
-	if ($result2 === false)
-	{
-		die('Error: Signed-off-by not found in commit message [' . $lastLine . ']' . '[' . $message . ']' . '[' . $debugSecondary . '][' . $debugMaster . ']' . "\n");
-	}
+	global $debugMsgs;
+
+	// Get verify commit data.
+	$message = trim(shell_exec('git verify-commit ' . $commit));
+
+	// Should we actually test for gpg results?  Perhaps, but it seems doing that with travis may fail since it has no way to verify a GPG signature from GitHub.  GitHub should have prevented a bad GPG from making a commit to a authors repository and could be trusted in most cases it seems.
+	$result = strlen($message) > 0;
+
+	// Debugger.
+	$debugMsgs[$commit . ':' . time()] = array(
+		// Raw body.
+		'verify-commit' => shell_exec('git verify-commit ' . $commit . ' -v --raw'),
+		// Result.
+		'result' => $result,
+		// Last tested string, or the correct string.
+		'message' => $message,
+	);
+
+	return $result;
+}
+
+// Print a debug line
+function debugPrint($msg)
+{
+	global $debugMode;
+
+	if ($debugMode)
+		echo "\nDEBUG: ", $msg;
 }
